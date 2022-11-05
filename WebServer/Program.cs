20177535abeb97d67e21e0059e2255b79a506d31
Ruslan <ruslan.khamzin712@gmail.com>
2022-11-05 13:13:03 +0300

    [mod] normal program.cs

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -11,19 +11,13 @@ namespace WebServer
 
         static void Main()
         {
-            //var result = new MyORM(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SteamDB;Integrated Security=True;")
-            var db = new MyORM(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SteamDB;Integrated Security=True;");
-            //db.ExecuteNonQuery("update Accounts set Login='Vanya', Password='4321' where Id=2");
-            //db.Update(new Account() { Id = 2, Login = "Vanya2", Password = "4321" });
-            //db.Delete<Account>(new Account() { Login = "New2", Password = "10239"});
-            db.Select<Account>().ForEach(i => Console.WriteLine(i.Id));
-            /*using var server = new HttpServer();
+            using var server = new HttpServer();
             server.Start();
 
             while (_appIsRunning)
             {
                 HandleCommand(Console.ReadLine()?.ToLower(), server);
-            }*/
+            }
         }
 
         private static void HandleCommand(string? command, HttpServer server)
